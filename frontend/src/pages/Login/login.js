fe313fe312aa6bb08091c490fce893cf92b63b33
NestorOtzx <nestorortiz017@gmail.com>
2025-03-07 11:16:20 -0500

    login first version

--- frontend/src/pages/Login/login.js
+++ frontend/src/pages/Login/login.js
@@ -1,13 +1,25 @@
 
+import { Link } from "react-router-dom";
 import './login.css'
 
 export default function Login()
 {
     return (
         <div className='login-container'>
-            <input placeholder="Document ID"></input>
-            <input placeholder="Password"></input>
-            <button type="submit">Login</button>
+            <div className='login-form'>
+                <Link to="/">
+                    {/*<FontAwesomeIcon icon={faIgloo} size="1x" className={styles.iconStyle} />*/}
+                    <img src="/POS_icon.png" alt="POS Logo" className="iconStyle" />
+                </Link>
+                <span className='form-title'>Point Of Sale System</span>
+                <span className='form-subtitle'>Login</span>
+                <div className="separator-line"></div>
+                <form className="form-container">
+                    <input placeholder="Document ID"></input>
+                    <input placeholder="Password"></input>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
         </div>
     );
 }
