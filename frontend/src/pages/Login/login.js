d17e3119bd748fea9fb1e17e87b514d1f6de4694
LauraFranco404 <laurafranco1116@gmail.com>
2025-03-11 15:00:42 -0500

    MOD Login

--- frontend/src/pages/Login/login.js
+++ frontend/src/pages/Login/login.js
@@ -31,7 +31,7 @@ export default function Login() {
             setError("Server error. Please try again later.");
         }
     };
-
+    /*
     return (
         <div className='login-container'>
             <div className='login-form'>
@@ -40,13 +40,52 @@ export default function Login() {
                 </Link>
                 <span className='form-title'>Login to Your Account</span>
                 <div className="separator-line"></div>
+                
                 <form className="form-container" onSubmit={handleSubmit}>
                     <input placeholder="Document ID" value={documentid} onChange={(e) => setDocumentID(e.target.value)} required />
                     <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                    
                     <button type="submit">Login</button>
                 </form>
                 {error && <div className="error-message">{error}</div>}
             </div>
         </div>
-    );
+    );*/
+    return(
+        <div className="login-container">
+            <div className="login-box">
+                {/*Sección izquierda*/}
+                <div className="left-section">
+                    <Link to="/">
+                        <img src="/POS_icon.png" alt="POS Logo" className="iconStyle" />
+                    </Link>    
+                    <h2 className="form-title">Login to Your Account</h2>
+                    {/* Formulario de login */}
+                    <form className="form-container" onSubmit={handleSubmit}>
+                        <input 
+                        placeholder="Document ID" value={documentid} onChange={(e) => setDocumentID(e.target.value)} required
+                        />
+
+                        <div className="password-container">
+                           <input 
+                           type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required
+                           />
+                        <button type="submit" className="login-button">Login</button>
+                        </div>
+
+                    </form>
+
+                    {error && <div className="error-message">{error}</div>}
+                </div>
+
+                {/* Sección derecha - Registro */}
+                <div className="right-section">
+                    <h2>New Here?</h2>
+                    <p>Ask the administrator for your credentials!</p>
+                </div>
+            </div>
+        </div>
+
+
+    )
 }
